ff0a26c274fdd467fe3d82e2e30b152bd20eeaf7
KennethGomez <kennethgomezfdz@gmail.com>
2021-04-07 12:11:47 +0200

    Implement on pointer move star alpha animation

--- src/app/canvas/ticker/space/star/star-interaction/star-interaction-ticker.ts
+++ src/app/canvas/ticker/space/star/star-interaction/star-interaction-ticker.ts
@@ -10,13 +10,38 @@ import { Star } from '../../../../space/star/star';
 import { AbstractTicker } from '../../../ticker.abstract';
 
 export class StarInteractionTicker extends AbstractTicker {
+    private readonly _oldAnimatingStars: Set<Star>;
+    private readonly _animatingStars: Set<Star>;
+
     public constructor() {
         super();
+
+        this._oldAnimatingStars = new Set();
+        this._animatingStars = new Set();
 
         Events.on(Event.SPACE_POINTER_MOVE, this._onSpacePointerOver, this);
     }
 
     public update(delta: number): void {
+        for (const animatingStar of this._oldAnimatingStars.values()) {
+            animatingStar.sprite.alpha -= 0.1;
+
+            const targetAlpha = animatingStar.brightness / 0xFF;
+
+            if (animatingStar.sprite.alpha <= targetAlpha) {
+                animatingStar.sprite.alpha = targetAlpha;
+
+                this._oldAnimatingStars.delete(animatingStar);
+            }
+        }
+
+        for (const animatingStar of this._animatingStars.values()) {
+            animatingStar.sprite.alpha += 0.1;
+
+            if (animatingStar.sprite.alpha >= 1) {
+                animatingStar.sprite.alpha = 1;
+            }
+        }
     }
 
     private _onSpacePointerOver(event?: INativeEvent<PIXI.InteractionEvent>) {
@@ -26,9 +51,7 @@ export class StarInteractionTicker extends AbstractTicker {
 
         const { base } = event;
 
-        for (const star of this._getClosePointChilds(base)) {
-            star.sprite.alpha = 1;
-        }
+        this._addAnimatingStars(this._getClosePointChilds(base));
     }
 
     private _getClosePointChilds(event: PIXI.InteractionEvent): Star[] {
@@ -37,7 +60,27 @@ export class StarInteractionTicker extends AbstractTicker {
         return App.instance.canvas.space.stars.filter(({ sprite: { x, y } }: Star) => {
             const distance = Math.sqrt(Math.abs(x - pX) ** 2 + Math.abs(y - pY) ** 2);
 
-            return distance < 30;
+            return distance < 40;
         });
+    }
+
+    private _addAnimatingStars(stars: Star[]) {
+        for (const oldStar of this._animatingStars) {
+            if (stars.includes(oldStar)) {
+                continue;
+            }
+
+            this._animatingStars.delete(oldStar);
+
+            if (!this._oldAnimatingStars.has(oldStar)) {
+                this._oldAnimatingStars.add(oldStar);
+            }
+        }
+
+        for (const star of stars) {
+            if (!this._animatingStars.has(star)) {
+                this._animatingStars.add(star);
+            }
+        }
     }
 }
