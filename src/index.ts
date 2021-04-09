d2cc59038942796d89a826834a784e244ef63116
KennethGómez <kennethgomezfdz@gmail.com>
2021-04-09 13:50:40 +0200

    Remove customElements stuff from index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,3 @@ import '../styles/index.sass';
 import { App } from './app/app';
 
 App.instance.init();
-
-class SpaceButton extends HTMLButtonElement {
-    public constructor() {
-        super();
-
-        this.addEventListener('click', this._onClick);
-    }
-
-    private _onClick() {
-        console.log('asd');
-    }
-}
-
-customElements.define('space-button', SpaceButton, { extends: 'button' });
